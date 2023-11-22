c4a7759a3c921b011ebe2495406f90575b0d0d2e
minecraft2.ggabz190516@gmail.com <minecraft2.ggabz190516@gmail.com>
2023-11-22 17:16:51 +0100

    Add Informations class & Update NetworkForm

---
+++ UltraTools/Informations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraTools
+{
+    internal class Informations
+    {
+        private static string version = "1.0.2-BETA";
+        private static string copyright = $"© {DateTime.Now.Year} UltraTools. Tous droits réservés.";
+        private static string author = "GabzDEV";
+
+        public string getVersion()
+        {
+            return version;
+        }
+
+        public string getCopyright()
+        {
+            return copyright;
+        }
+
+        public string getAuthor()
+        {
+            return author;
+        }
+    }
+}
