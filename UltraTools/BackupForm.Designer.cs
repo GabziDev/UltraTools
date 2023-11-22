f7783ea9676f7c58f48355acc7489ed904b44333
minecraft2.ggabz190516@gmail.com <minecraft2.ggabz190516@gmail.com>
2023-11-22 17:33:49 +0100

    Add style BackupForm

--- UltraTools/BackupForm.Designer.cs
+++ UltraTools/BackupForm.Designer.cs
@@ -1,5 +1,7 @@
-﻿namespace UltraTools {
-    partial class BackupForm {
+﻿namespace UltraTools
+{
+    partial class BackupForm
+    {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -26,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "BackupForm";
+            panelBottom = new Panel();
+            panelMiddle = new Panel();
+            panelTop = new Panel();
+            SuspendLayout();
+            // 
+            // panelBottom
+            // 
+            panelBottom.BackColor = Color.FromArgb(38, 39, 70);
+            panelBottom.Location = new Point(12, 300);
+            panelBottom.Name = "panelBottom";
+            panelBottom.Size = new Size(776, 138);
+            panelBottom.TabIndex = 20;
+            // 
+            // panelMiddle
+            // 
+            panelMiddle.BackColor = Color.FromArgb(38, 39, 70);
+            panelMiddle.Location = new Point(12, 156);
+            panelMiddle.Name = "panelMiddle";
+            panelMiddle.Size = new Size(776, 138);
+            panelMiddle.TabIndex = 19;
+            // 
+            // panelTop
+            // 
+            panelTop.BackColor = Color.FromArgb(38, 39, 70);
+            panelTop.Location = new Point(12, 12);
+            panelTop.Name = "panelTop";
+            panelTop.Size = new Size(776, 138);
+            panelTop.TabIndex = 18;
+            // 
+            // BackupForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(46, 46, 83);
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelBottom);
+            Controls.Add(panelMiddle);
+            Controls.Add(panelTop);
+            Name = "BackupForm";
+            Text = "BackupForm";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelBottom;
+        private Panel panelMiddle;
+        private Panel panelTop;
     }
 }
