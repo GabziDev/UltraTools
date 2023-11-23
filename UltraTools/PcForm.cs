590faa4ae9d1de159aea58012c32abd52729b03f
souciss <nelson.antns@eduge.ch>
2023-11-23 09:37:29 +0100

    Revert "ajout fonctionnalite onglet pc"
    This reverts commit 44e6ca0d6854900b59534f60ce1d7cdc9596b060.

--- UltraTools/PcForm.cs
+++ UltraTools/PcForm.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-namespace UltraTools
+﻿namespace UltraTools
 {
     public partial class PcForm : Form
     {
@@ -15,25 +13,6 @@ namespace UltraTools
             Informations informations = new Informations();
             Text = informations.TitleForm("PC");
             Icon = Properties.Resources.logo;
-            //ram 
-            PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available Bytes");
-            double availableBytes = ramCounter.NextValue();
-            double availableGigaBytes = availableBytes / 1073741824;
-            availableGigaBytes = Math.Round(availableGigaBytes, 1);
-
-            //stockage
-            DriveInfo driveInfo = new DriveInfo("C:");
-            double stockageRestant = driveInfo.AvailableFreeSpace / (1024.0 * 1024.0 * 1024.0);
-            stockageRestant = Math.Round(stockageRestant, 1);
-
-
-
-
-
-
-            lblStockage.Text = ("il reste en stockage " + stockageRestant + " GB de libre ");
-            lblRam.Text = ("il reste en ram" + availableGigaBytes + " GB de libre ");
-
         }
 
         private void PcForm_Load(object sender, EventArgs e)
@@ -45,11 +24,6 @@ namespace UltraTools
             // Afficher PopUp
             popUpInstance = new PopUp();
             popUpInstance.Dev();
-        }
-
-        private void panelTop_Paint(object sender, PaintEventArgs e)
-        {
-
         }
     }
 }
