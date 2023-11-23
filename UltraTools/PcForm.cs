4d8d2d4774b1d4229715cb0066e0fab64719e8c1
GabziDev <minecraf2.ggabz190516@gmail.com>
2023-11-23 11:13:42 +0100

    Add IP Gateway

--- UltraTools/PcForm.cs
+++ UltraTools/PcForm.cs
@@ -4,6 +4,7 @@ namespace UltraTools {
     public partial class PcForm : Form {
         // Instance
         private PopUp popUpInstance;
+        private System.Windows.Forms.Timer timer;
 
         public PcForm()
         {
@@ -17,8 +18,30 @@ namespace UltraTools {
 
         private void PcForm_Load(object sender, EventArgs e)
         {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 500;
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+
+            // Ajouter un footer
+            Informations informations = new Informations();
+            labelFooter.Text = $"{informations.getAuthor()} - {informations.getCopyright()} - {informations.getVersion()}";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ShowInformations();
+        }
+
+        private void ShowInformations()
+        {
             try
             {
+                //cpu
+                PerformanceCounter cpuCounter;
+                cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
                 //ram 
                 PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available Bytes");
                 double availableBytes = ramCounter.NextValue();
@@ -30,7 +53,7 @@ namespace UltraTools {
                 DriveInfo driveInfo = new DriveInfo("C:");
                 double stockageRestant = driveInfo.AvailableFreeSpace / (1024.0 * 1024.0 * 1024.0);
                 stockageRestant = Math.Round(stockageRestant, 1);
-                labelStorage.Text = ("Stockage Libre : " + stockageRestant + "Go");
+                labelStorage.Text = ("Stockage Libre : " + stockageRestant + "Go -->" + cpuCounter.NextValue() + "%");
             }
             catch (Exception ex)
             {
@@ -40,9 +63,6 @@ namespace UltraTools {
                 popUpInstance.Erreur(erreur);
             }
 
-            // Ajouter un footer
-            Informations informations = new Informations();
-            labelFooter.Text = $"{informations.getAuthor()} - {informations.getCopyright()} - {informations.getVersion()}";
         }
     }
 }
