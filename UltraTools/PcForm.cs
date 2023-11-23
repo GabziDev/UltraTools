fca32a4bf38d16b3a7b72f265101694f240367ff
souciss <nelson.antns@eduge.ch>
2023-11-23 15:59:20 +0100

    ajout terminale section pc

--- UltraTools/PcForm.cs
+++ UltraTools/PcForm.cs
@@ -1,4 +1,8 @@
 ﻿using System.Diagnostics;
+using System.Globalization;
+using System.Security.Principal;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace UltraTools
 {
@@ -42,6 +46,8 @@ namespace UltraTools
                 double stockageRestant = driveInfo.AvailableFreeSpace / (1024.0 * 1024.0 * 1024.0);
                 stockageRestant = Math.Round(stockageRestant, 1);
                 labelStorage.Text = ("Stockage Libre : " + stockageRestant + "Go");
+
+
             }
             catch (Exception ex)
             {
@@ -52,5 +58,32 @@ namespace UltraTools
             }
 
         }
+
+        private void btnShutdown_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 0 /f");
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/r /t 0 /f");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string command = rtboxEntreTerminale.Text;
+
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c " + command;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            rtboxEntreTerminale.Text = output;
+            process.WaitForExit();
+
+        }
+
     }
 }
