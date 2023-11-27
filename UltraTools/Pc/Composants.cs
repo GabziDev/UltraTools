1f7d06a41326be2f81c3d068a5b41eb4d9e6c792
GabziDev <minecraf2.ggabz190516@gmail.com>
2023-11-27 11:07:14 +0100

    Update Composants CLASS

--- UltraTools/Pc/Composants.cs
+++ UltraTools/Pc/Composants.cs
@@ -1,4 +1,19 @@
 ﻿namespace UltraTools.Pc {
     internal class Composants {
+        // Variables
+        private static string cpuName = System.Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+        private static string gpuName = "null";
+
+
+        // Get
+        public string getCpuName()
+        {
+            return cpuName;
+        } 
+
+        public string getGpuName()
+        {
+            return gpuName;
+        }
     }
 }
