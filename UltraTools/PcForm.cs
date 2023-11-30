dc63ee2a4ba361d2e75537148e1f86197a9f740e
souciss <nelson.antns@eduge.ch>
2023-11-30 14:11:57 +0100

    ajout pc nom utilisateur et opti cmd

--- UltraTools/PcForm.cs
+++ UltraTools/PcForm.cs
@@ -2,8 +2,10 @@
 using UltraTools.Common;
 using UltraTools.Pc;
 
-namespace UltraTools {
-    public partial class PcForm : Form {
+namespace UltraTools
+{
+    public partial class PcForm : Form
+    {
         // Instances
         private PopUp popUpInstance;
 
@@ -30,27 +32,63 @@ namespace UltraTools {
 
         private async void ShowInformations()
         {
+
+
+
+
+
+
+
+
             try
             {
                 double availableGigaBytes = 0;
                 double stockageRestant = 0;
+                string result = "error";
 
+           
+               
                 await Task.Run(() =>
                 {
+
+                    Process process = new Process();
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.Arguments = "/c " + "systeminfo";
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.CreateNoWindow = true; // Ajout de cette ligne pour cacher la fenêtre du terminal
+                    process.Start();
+                    // skip ligne 2
+                    process.StandardOutput.ReadLine();
+                    process.StandardOutput.ReadLine();
+
+                    // lis ligne 
+                    string output = process.StandardOutput.ReadLine();
+                    string toRemove = "Nom du systŠme d’exploitation:         ";
+                    Console.WriteLine(output);
+                    result = output.Substring(toRemove.Length);
+                    process.WaitForExit();
+
+
                     // RAM
                     PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available Bytes");
                     double availableBytes = ramCounter.NextValue();
                     availableGigaBytes = availableBytes / 1073741824;
                     availableGigaBytes = Math.Round(availableGigaBytes, 1);
 
+
                     // Stockage
                     DriveInfo driveInfo = new DriveInfo("C:");
                     stockageRestant = driveInfo.AvailableFreeSpace / (1024.0 * 1024.0 * 1024.0);
                     stockageRestant = Math.Round(stockageRestant, 1);
+
+
                 });
 
+                lblNomOs.Text = "version windows" + result;
                 labelRAM.Text = ("R.A.M. Libre : " + availableGigaBytes + "Go");
                 labelStorage.Text = ("Stockage Libre (OS) : " + stockageRestant + "Go");
+
             }
             catch (Exception ex)
             {
@@ -92,6 +130,24 @@ namespace UltraTools {
             string output = process.StandardOutput.ReadToEnd();
             rtboxEntreTerminale.Text = output;
             process.WaitForExit();
+        }
+
+        private void BtnNetUser_Click(object sender, EventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c " + "net user ";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            rtboxEntreTerminale.Text = output;
+            process.WaitForExit();
+        }
+
+        private void lblNomOs_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
