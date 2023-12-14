d1c9326caf4f306424149e609d7f82e5e9d2fea8
gabzdev <gabz@gabzdev.ch>
2023-12-14 14:50:31 +0100

    Change version

--- UltraTools/Common/Informations.cs
+++ UltraTools/Common/Informations.cs
@@ -2,7 +2,7 @@
 {
     internal class Informations
     {
-        private static string version = "1.0.7-BETA";
+        private static string version = "1.0.7-BETA DEV VERSION";
         private static string copyright = $"© {DateTime.Now.Year} UltraTools. Tous droits réservés.";
         private static string[] author = { "GabzDEV", ", Souciss" };
 
